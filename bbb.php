6be1366209ab3b1f300250a1597aab4c02011825
ferdiben <ferdinando.beniamino95@live.com>
2017-08-25 18:13:42 +0200

    Update bbb.php

--- bbb.php
+++ bbb.php
@@ -1,11 +1,6 @@
 
 <?php
-$client = new MongoClient ( 
-    'mongodb://SvensonTeam:Capracotta.1@ds157833.mlab.com:57833/annunciauto');
-                       $a = $client->annunciauto;
-					   $b = $a->Auto;
-					   $c =$b->find();
-       var_dump(iterator_to_array($c));  
+
 echo("ciao mondo");	   
     
 ?>
