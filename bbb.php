5a71be5dd50d6f45a55a9f169d31030316a2950b
ferdiben <ferdinando.beniamino95@live.com>
2017-08-25 19:47:10 +0200

    Update bbb.php

--- bbb.php
+++ bbb.php
@@ -15,7 +15,7 @@ foreach($c as $doc){
 use Telegram\Bot\Api;
 
 $telegram = new Api('323852343:AAH5AZvSM5ceC60KSKIFVV-dHzHQgA7JnJg');
-$response = $telegram->getMe();
+$response = "vaffanculo";
 echo($response);
 $botId = $response->getId();
 $firstName = $response->getFirstName();
