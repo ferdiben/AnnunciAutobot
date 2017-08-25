2f4710b77856990caf601d1ffed2365bae23b7bd
ferdiben <ferdinando.beniamino95@live.com>
2017-08-25 19:23:37 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://Abcgbot.herokuapp.com/execute.php';
+$BOT_TOKEN = '323852343:AAH5AZvSM5ceC60KSKIFVV-dHzHQgA7JnJg';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
