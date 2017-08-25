1c7f305720dd2cf08068ad1df7e375272dfcf398
ferdiben <ferdinando.beniamino95@live.com>
2017-08-25 18:42:22 +0200

    Update bbb.php

--- bbb.php
+++ bbb.php
@@ -1,13 +1,11 @@
-
 <?php
-
 $client = new MongoClient ( 
     'mongodb://SvensonTeam:Capracotta.1@ds157833.mlab.com:57833/annunciauto');
                        $a = $client->annunciauto;
 					   $b = $a->Auto;
 					   $c =$b->find();
 foreach($c as $doc){
-       echo(iterator_to_array($doc));    
+       var_dump($doc);    
 }
     
 ?>
