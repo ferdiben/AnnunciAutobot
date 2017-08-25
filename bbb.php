2eda4ab3a78c7ae68b40f57f525af6f46a2b2e47
ferdiben <ferdinando.beniamino95@live.com>
2017-08-25 19:13:37 +0200

    Update bbb.php

--- bbb.php
+++ bbb.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-
+require 'vendor/autoload.php';
 $client = new Mongo ( 
     'mongodb://SvensonTeam:Capracotta.1@ds157833.mlab.com:57833/annunciauto');
                        $a = $client->annunciauto;
