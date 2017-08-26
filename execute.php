6355f1a85d7476a120c363507e45ff68a6ac68e0
ferdiben <ferdinando.beniamino95@live.com>
2017-08-26 18:04:22 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -31,9 +31,32 @@ header("Content-Type: application/json");
 ;
 // converto e stampo l'array JSON sulla response
 
-    $parameters = array('chat_id' => $chatId, "text" => "ciao".$text);
-    $parameters["method"] = "sendMessage";
-echo json_encode($parameters);
+ //   $parameters = array('chat_id' => $chatId, "text" => "ciao".$text);
+//    $parameters["method"] = "sendMessage";
+//echo json_encode($parameters);
+
+
+$keyboard = [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+         ['0']
+];
+
+$reply_markup = $telegram->replyKeyboardMarkup([
+    'keyboard' => $keyboard, 
+    'resize_keyboard' => true, 
+    'one_time_keyboard' => true
+]);
+
+$response = $telegram->sendMessage([
+    'chat_id' => $chatId, 
+    'text' => 'Hello World', 
+    'reply_markup' => $reply_markup
+]);
+
+$messageId = $response->getMessageId();
+
 
 
 
