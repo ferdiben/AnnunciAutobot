7ec7be14bf6b9b30fce45f9e44ce1c79a0bacbce
federicosozio <sozio.federico@gmail.com>
2017-08-26 16:12:11 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -10,11 +10,7 @@ if(!$update)
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
-$date = isset($message['date']) ? $message['date'] : "";
-$text = isset($message['text']) ? $message['text'] : "";
+
 
 $text = trim($text);
 $text = strtolower($text);
