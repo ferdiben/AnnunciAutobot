15935a3a155cc7f47b022009f64531e7c45ef49b
federicosozio <sozio.federico@gmail.com>
2017-08-27 17:13:07 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -51,7 +51,7 @@ $parameters = array('chat_id' => $chatId, "text" => $marche[1]);
 $parameters["method"] = "sendMessage";
 
 // imposto la keyboard
-$parameters["reply_markup"] = '{ "keyboard": [["fff"], ["ssss"], [(string)$r], "one_time_keyboard": false}';
+$parameters["reply_markup"] = '{ "keyboard": [["fff"], ["ssss"], $marche[1], "one_time_keyboard": false}';
 // converto e stampo l'array JSON sulla response
 echo json_encode($parameters);
 
