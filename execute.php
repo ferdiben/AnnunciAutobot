14480ab759b7a42064c9963b9ddb103dd42f124a
federicosozio <sozio.federico@gmail.com>
2017-08-27 17:48:49 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -52,7 +52,7 @@ $parameters = array('chat_id' => $chatId, "text" => $marche[1]);
 // method è il metodo per l'invio di un messaggio (cfr. API di Telegram)
 $parameters["method"] = "sendMessage";
 // imposto la keyboard
-$parameters["reply_markup"] = '{ "keyboard":[echo($r), ["ffff"]], "one_time_keyboard": false}';
+$parameters["reply_markup"] = '{ "keyboard":[[echo('$r')], ["ffff"]], "one_time_keyboard": false}';
 // converto e stampo l'array JSON sulla response
 echo json_encode($parameters);
 
