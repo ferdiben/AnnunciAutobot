f3cc62b03e7d6758c9ccddeaf09bf30ae3d6be6a
federicosozio <sozio.federico@gmail.com>
2017-08-28 17:53:03 +0200

    Update update.php

--- update.php
+++ update.php
@@ -20,11 +20,7 @@ foreach ($cursor as $key) {
 
 $option = array( 
     //First row
-    array($telegram->buildKeyboardButton($marche[1]), $telegram->buildKeyboardButton("Button 2")), 
-    //Second row 
-    array($telegram->buildKeyboardButton("Button 3"), $telegram->buildKeyboardButton("Button 4"), $telegram->buildKeyboardButton("Button 5")), 
-    //Third row
-    array($telegram->buildKeyboardButton("Button 6")) );
+    $marche );
 $keyb = $telegram->buildKeyBoard($option, $onetime=false);
 $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "This is a Keyboard Test");
 $telegram->sendMessage($content);
