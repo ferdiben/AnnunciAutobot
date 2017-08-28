58b64446ec47e64fa26b5360a3872976e440226c
federicosozio <sozio.federico@gmail.com>
2017-08-28 17:51:06 +0200

    Update update.php

--- update.php
+++ update.php
@@ -5,9 +5,22 @@ $bot_token = '323852343:AAH5AZvSM5ceC60KSKIFVV-dHzHQgA7JnJg';
 $telegram = new Telegram($bot_token);
 $chat_id = $telegram->ChatID();
 
+  $connection = new MongoClient('mongodb://SvensonTeam:Capracotta.1@ds157833.mlab.com:57833/annunciauto');
+    $database   = $connection->selectDB('annunciauto');
+    $collection = $database->selectCollection('Marche_Modelli');
+
+
+$i = 0;
+$cursor = $collection->find();
+foreach ($cursor as $key) {
+    $marche[$i] = $key['marca'];
+  $i++;
+  var_dump($marche);
+}
+
 $option = array( 
     //First row
-    array($telegram->buildKeyboardButton("Button 1"), $telegram->buildKeyboardButton("Button 2")), 
+    array($telegram->buildKeyboardButton($marche[1]), $telegram->buildKeyboardButton("Button 2")), 
     //Second row 
     array($telegram->buildKeyboardButton("Button 3"), $telegram->buildKeyboardButton("Button 4"), $telegram->buildKeyboardButton("Button 5")), 
     //Third row
