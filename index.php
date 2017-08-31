2f9bed32d1e6b18070012c189e94b59fd6a1c44c
ferdiben <ferdinando.beniamino95@live.com>
2017-08-31 16:33:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?php
 error_reporting(E_ALL);
 
-$RedisClient = new Redis ();
-
-$RedisClient ->connect( “tcp://ec2-34-252-182-25.eu-west-1.compute.amazonaws.com”, “13419”, “NULL”, “150”);
-
-
- 
-session_start();
- 
-$count = isset($_SESSION['count']) ? $_SESSION['count'] : 1;
- 
-echo $count;
- 
-$_SESSION['count'] = ++$count;
+ if($_ENV['REDIS_URL']) {
+    $redisUrlParts = parse_url($_ENV['REDIS_URL']);
+    ini_set('session.save_handler','redis');
+    ini_set('session.save_path',"tcp://$redisUrlParts[ec2-34-252-182-25.eu-west-1.compute.amazonaws.com]:$redisUrlParts[13419]?auth=$redisUrlParts[p05ebe76c4296539328f91efde721822040f16c9e599be903602914d21c27a55e]");
+  }
  
 ?>
