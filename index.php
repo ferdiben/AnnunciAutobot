c7db07d4f8d10afbb8b6aba79dd4e225ae11ab6d
ferdiben <ferdinando.beniamino95@live.com>
2017-08-31 16:16:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('session.save_handler', 'redis');
  
-ini_set('session.save_path', "ec2-34-252-182-25.eu-west-1.compute.amazonaws.com:13419");
+ini_set('session.save_path', "redis://h:p05ebe76c4296539328f91efde721822040f16c9e599be903602914d21c27a55e@ec2-34-252-182-25.eu-west-1.compute.amazonaws.com:13419");
  
 //echo ini_get('session.save_path');
  
