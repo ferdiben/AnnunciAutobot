84e0225e95452ee32c292989d2cf1dedde5e5ba5
ferdiben <ferdinando.beniamino95@live.com>
2017-08-31 16:07:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php echo "Silence is golden";
+<?php
+ 
+ini_set('session.save_handler', 'redis');
+ 
+ini_set('session.save_path', "ec2-34-252-182-25.eu-west-1.compute.amazonaws.com:13419");
+ 
+//echo ini_get('session.save_path');
+ 
+session_start();
+ 
+$count = isset($_SESSION['count']) ? $_SESSION['count'] : 1;
+ 
+echo $count;
+ 
+$_SESSION['count'] = ++$count;
+ 
+?>
