89b227b4172c8da35cf3a0467e49631895fa9b3f
ferdiben <ferdinando.beniamino95@live.com>
2017-08-31 17:39:29 +0200

    Update update.php

--- update.php
+++ update.php
@@ -2,11 +2,7 @@
 include 'Telegram.php';
 include 'find.php';
 
-error_reporting(E_ALL);
-    ini_set('session.save_handler','redis');
-    ini_set('session.save_path',"tcp://ec2-34-252-182-25.eu-west-1.compute.amazonaws.com:13419?auth=p05ebe76c4296539328f91efde721822040f16c9e599be903602914d21c27a55e");
-  
-
+error_reporting(E_ALL);  
 // Set the bot TOKEN
 $bot_token = '323852343:AAH5AZvSM5ceC60KSKIFVV-dHzHQgA7JnJg';
 $telegram = new Telegram($bot_token);
