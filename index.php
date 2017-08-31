f0e05713aca9bff58931f78d63ceb17f69b2b49f
ferdiben <ferdinando.beniamino95@live.com>
2017-08-31 17:35:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<?php
-error_reporting(E_ALL);
+ <?php
+//simple counter to test sessions. should increment on each page reload.
 session_start();
-session_destroy();
- if($_ENV['REDIS_URL']) {
-    $redisUrlParts = parse_url($_ENV['REDIS_URL']);
-    ini_set('session.save_handler','redis');
-    ini_set('session.save_path',"tcp://$redisUrlParts[ec2-34-252-182-25.eu-west-1.compute.amazonaws.com]:$redisUrlParts[13419]?auth=$redisUrlParts[p05ebe76c4296539328f91efde721822040f16c9e599be903602914d21c27a55e]");
-  }
- 
-?>
+$count = isset($_SESSION['count']) ? $_SESSION['count'] : 1;
+
+echo $count;
+
+$_SESSION['count'] = ++$count;
