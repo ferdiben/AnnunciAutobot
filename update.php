220d2a7d56d775b35100f75d153bd243b66e7a31
ferdiben <ferdinando.beniamino95@live.com>
2017-08-31 16:37:24 +0200

    Update update.php

--- update.php
+++ update.php
@@ -3,11 +3,9 @@ include 'Telegram.php';
 include 'find.php';
 
 error_reporting(E_ALL);
- if($_ENV['REDIS_URL']) {
-    $redisUrlParts = parse_url($_ENV['REDIS_URL']);
     ini_set('session.save_handler','redis');
-    ini_set('session.save_path',"tcp://$redisUrlParts[ec2-34-252-182-25.eu-west-1.compute.amazonaws.com]:$redisUrlParts[13419]?auth=$redisUrlParts[p05ebe76c4296539328f91efde721822040f16c9e599be903602914d21c27a55e]");
-  }
+    ini_set('session.save_path',"tcp://ec2-34-252-182-25.eu-west-1.compute.amazonaws.com:13419?auth=p05ebe76c4296539328f91efde721822040f16c9e599be903602914d21c27a55e");
+  
 
 $RedisClient = new Redis ();
 $RedisClient ->connect( "tcp://ec2-34-252-182-25.eu-west-1.compute.amazonaws.com", "13419", "NULL", "150");
