551981bf7a4b88da8441930134da390f2f33a3bd
federicosozio <sozio.federico@gmail.com>
2017-09-01 17:58:39 +0200

    Update update.php

--- update.php
+++ update.php
@@ -8,7 +8,7 @@ $bot_token = '323852343:AAH5AZvSM5ceC60KSKIFVV-dHzHQgA7JnJg';
 $telegram = new Telegram($bot_token);
 $chat_id = $telegram->ChatID();
 $result = $telegram->getData();
-$callback_query = $telegram->Callback_ID();
+$callback_query = $telegram->Callback_Message();
 
 $text = $result["message"]["text"];
 $text = trim($text);
