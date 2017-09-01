bd7b3fe5f14d37f40a4c13126f21dfeacfaa919d
ferdiben <ferdinando.beniamino95@live.com>
2017-09-01 18:18:03 +0200

    Update update.php

--- update.php
+++ update.php
@@ -8,8 +8,9 @@ $bot_token = '323852343:AAH5AZvSM5ceC60KSKIFVV-dHzHQgA7JnJg';
 $telegram = new Telegram($bot_token);
 $chat_id = $telegram->ChatID();
 $result = $telegram->getData();
+$b = $result->get_Message();
 $callback_query = $telegram->Callback_Message();
-$a = $callback_query['message'];
+$a = $callback_query['message'].get('text');
 
 $text = $result["message"]["text"];
 $text = trim($text);
@@ -27,7 +28,9 @@ $keyb = $telegram->buildInlineKeyBoard($option);
 if ($text === "/start" || (!isset($_SESSION["marca"]) && !isset($_SESSION["modello"]) && !isset($_SESSION["regione"]) && !isset($_SESSION["provincia"]) && !isset($_SESSION["alimentazione"]))){
     $content = array('chat_id' => $chat_id, 'text' => "Benvenuto! Inserisci l'auto da cercare");
 } else {
-$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $questions[0].$a);
+//$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $questions[0].$a);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $b);
+
 }
 $telegram->sendMessage($content);
 ?>
