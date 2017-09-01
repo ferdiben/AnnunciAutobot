e842c785ef6b2f3ad713d1c71694504460aa2c49
ferdiben <ferdinando.beniamino95@live.com>
2017-09-01 17:41:31 +0200

    Update update.php

--- update.php
+++ update.php
@@ -10,6 +10,11 @@ $chat_id = $telegram->ChatID();
 $result = $telegram->getData();
 $callback_query = $telegram->Callback_Query();
 
+$update = $this->getUpdate();
+$callback_query = $update->getCallbackQuery();
+$callback_query_id = $callback_query->getId();
+$callback_datas = $callback_query->getData();
+
 $text = $result["message"]["text"];
 $text = trim($text);
 $text = strtolower($text);
@@ -26,7 +31,7 @@ $keyb = $telegram->buildInlineKeyBoard($option);
 if ($text === "/start" || (!isset($_SESSION["marca"]) && !isset($_SESSION["modello"]) && !isset($_SESSION["regione"]) && !isset($_SESSION["provincia"]) && !isset($_SESSION["alimentazione"]))){
     $content = array('chat_id' => $chat_id, 'text' => "Benvenuto! Inserisci l'auto da cercare");
 } else {
-$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $questions[0].$callback_data.$callback_query['callback_data1']);
+$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $questions[0].$callback_datas.$callback_query['callback_data1']);
 }
 $telegram->sendMessage($content);
 ?>
