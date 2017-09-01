ac5366b3581d68c675e057898e9e507a25d3fcba
federicosozio <sozio.federico@gmail.com>
2017-09-01 21:18:38 +0200

    Update update.php

--- update.php
+++ update.php
@@ -28,7 +28,7 @@ $questions = setParametri();
 if($callback_query["data"] === "eseguiricerca"){
         //session_destroy();
 } elseif($callback_query["data"] === "skip"){
-   unset($_SESSION['total_elements']);
+   $_SESSION['total_elements'][0] = NULL;
 
 }
 
