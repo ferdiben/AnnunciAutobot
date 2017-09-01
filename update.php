d8622c19d0d106a81bf033519790ee0f24e1bc8a
ferdiben <ferdinando.beniamino95@live.com>
2017-09-01 19:08:47 +0200

    Update update.php

--- update.php
+++ update.php
@@ -8,7 +8,7 @@ $bot_token = '323852343:AAH5AZvSM5ceC60KSKIFVV-dHzHQgA7JnJg';
 $telegram = new Telegram($bot_token);
 $chat_id = $telegram->ChatID();
 $result = $telegram->getData();
-$b =  $result["message"]["data"];
+$b =  $result["chat_instance"]["data"];
 $callback_query = $telegram->Callback_Message();
 
 
