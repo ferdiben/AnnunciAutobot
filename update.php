2cd2d9e0b894308c0f0537071dd3e98504635f23
federicosozio <sozio.federico@gmail.com>
2017-09-01 19:15:24 +0200

    Update update.php

--- update.php
+++ update.php
@@ -8,16 +8,14 @@ $bot_token = '323852343:AAH5AZvSM5ceC60KSKIFVV-dHzHQgA7JnJg';
 $telegram = new Telegram($bot_token);
 $chat_id = $telegram->ChatID();
 $result = $telegram->getData();
-$b =  $telegram["callback_query"]["data"];
-$callback_query = $telegram->Callback_Message();
+$b =  $result["message"]["reply_markup"];
+$callback_query = $telegram->Callback_Query()->getData();
 
 
-ob_start();
-var_dump($telegram);
-$str = ob_get_clean();
+
 $file = 'file.txt';
 $current = file_get_contents($file);
-$current .= $str;
+$current .= "ciao";
 file_put_contents($file, $current);
 
 
@@ -37,7 +35,7 @@ $keyb = $telegram->buildInlineKeyBoard($option);
 if ($text === "/start" || (!isset($_SESSION["marca"]) && !isset($_SESSION["modello"]) && !isset($_SESSION["regione"]) && !isset($_SESSION["provincia"]) && !isset($_SESSION["alimentazione"]))){
     $content = array('chat_id' => $chat_id, 'text' => "Benvenuto! Inserisci l'auto da cercare");
 } else {
-$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $questions[0].$b);
+$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $questions[0].$callback_query);
 
 }
 $telegram->sendMessage($content);
