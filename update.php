4aed93370bde383748fba5a0a0cdb3732ddb24aa
federicosozio <sozio.federico@gmail.com>
2017-09-01 19:42:06 +0200

    Update update.php

--- update.php
+++ update.php
@@ -29,7 +29,7 @@ $questions = setParametri();
 if($callback_query["data"] === "eseguiricerca"){
         session_destroy();
 } elseif($callback_query["data"] === "skip"){
-    array_pop($_SESSION['total_elements']);
+    array_pop($_SESSION['total_elements'][0]);
 }
 
 $option = array( 
@@ -41,7 +41,7 @@ $keyb = $telegram->buildInlineKeyBoard($option);
 if ($text === "/start" || (!isset($_SESSION["marca"]) && !isset($_SESSION["modello"]) && !isset($_SESSION["regione"]) && !isset($_SESSION["provincia"]) && !isset($_SESSION["alimentazione"]))){
     $content = array('chat_id' => $chat_id, 'text' => "Benvenuto! Inserisci l'auto da cercare");
 } else {
-$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Parametri Ricerca:"."Marca:".$_SESSION["marca"].$_SESSION['total_elements'][0]);
+$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Parametri Ricerca:"."Marca:".$_SESSION["marca"]."Modello:".$_SESSION["modello"]."Regione:".$_SESSION["regione"]."Provincia:".$_SESSION["provincia"]."Alimentazione:".$_SESSION["alimentazione"].$_SESSION['total_elements'][0]);
 
 }
 $telegram->sendMessage($content);
