124acb2cebccdeaccc8abe21ae4736f07bbacecf
federicosozio <sozio.federico@gmail.com>
2017-09-01 18:00:00 +0200

    Update update.php

--- update.php
+++ update.php
@@ -26,7 +26,7 @@ $keyb = $telegram->buildInlineKeyBoard($option);
 if ($text === "/start" || (!isset($_SESSION["marca"]) && !isset($_SESSION["modello"]) && !isset($_SESSION["regione"]) && !isset($_SESSION["provincia"]) && !isset($_SESSION["alimentazione"]))){
     $content = array('chat_id' => $chat_id, 'text' => "Benvenuto! Inserisci l'auto da cercare");
 } else {
-$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $questions[0].$callback_query);
+$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $questions[0].$callback_query[0]);
 }
 $telegram->sendMessage($content);
 ?>
