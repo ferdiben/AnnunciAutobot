58332c54babba80e31325035f58e3a7e10d673da
federicosozio <sozio.federico@gmail.com>
2017-09-01 16:55:11 +0200

    Update update.php

--- update.php
+++ update.php
@@ -8,6 +8,8 @@ $bot_token = '323852343:AAH5AZvSM5ceC60KSKIFVV-dHzHQgA7JnJg';
 $telegram = new Telegram($bot_token);
 $chat_id = $telegram->ChatID();
 $result = $telegram->getData();
+$update = $this->getUpdate();
+$callback_query = $update->getCallbackQuery();
 $text = $result["message"]["text"];
 $text = trim($text);
 $text = strtolower($text);
@@ -24,7 +26,7 @@ $keyb = $telegram->buildInlineKeyBoard($option);
 if ($text === "/start" || (!isset($_SESSION["marca"]) && !isset($_SESSION["modello"]) && !isset($_SESSION["regione"]) && !isset($_SESSION["provincia"]) && !isset($_SESSION["alimentazione"]))){
     $content = array('chat_id' => $chat_id, 'text' => "Benvenuto! Inserisci l'auto da cercare");
 } else {
-    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $questions[0].$callback_data);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $questions[0].$callback_data.$callback_query);
 }
 $telegram->sendMessage($content);
 ?>
