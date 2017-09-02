3a110f4622207b85d8f93ffe83b2e1b03bd6c429
federicosozio <sozio.federico@gmail.com>
2017-09-02 19:03:17 +0200

    Update update.php

--- update.php
+++ update.php
@@ -24,7 +24,7 @@ Parametri($text, $chat_id);
 
 $questions = setParametri();
 
- if($callback_query["data"] === "skip" || $callback_query["data"] === NULL){
+ if($callback_query["data"] === "skip"){
     $_SESSION["$i"]++;
 } else{
     session_destroy();
