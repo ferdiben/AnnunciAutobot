c7e5e6822ddb3bd82bf23a066c502e68f998f8cc
federicosozio <sozio.federico@gmail.com>
2017-09-03 17:16:48 +0200

    Update update.php

--- update.php
+++ update.php
@@ -8,9 +8,10 @@ $bot_token = '323852343:AAH5AZvSM5ceC60KSKIFVV-dHzHQgA7JnJg';
 $telegram = new Telegram($bot_token);
 $chat_id = $telegram->ChatID();
 $result = $telegram->getData();
-$username = $result["message"]["first_name"];
+$username = $telegram->Username();
 $callback_query = $telegram->Callback_Query();
 $a = $callback_query["data"];
+
 
 $file = 'file.txt';
 $current = file_get_contents($file);
@@ -38,7 +39,7 @@ $keyb = $telegram->buildInlineKeyBoard($option);
 
 if ($text === "/start" || $text === "/nuova_ricerca" || $callback_query["data"] === "new" || (!isset($_SESSION["marca"]) && !isset($_SESSION["modello"]) && !isset($_SESSION["regione"]) && !isset($_SESSION["provincia"]) && !isset($_SESSION["alimentazione"]) && !isset($_SESSION["prezzo"]))){
         $_SESSION["$i"]=0;
-    $content = array('chat_id' => $chat_id, 'text' => "Ciao ".$username." ! Inserisci l'auto da cercare");
+    $content = array('chat_id' => $chat_id, 'text' => "Ciao ".$username["username"]." ! Inserisci l'auto da cercare");
 } else {
 $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Parametri Ricerca:"."\nMarca:  ".ucfirst($_SESSION["marca"])."\nModello:  ".ucfirst($_SESSION["modello"])."\nRegione:  ".ucfirst($_SESSION["regione"])."\nProvincia:  ".ucfirst($_SESSION["provincia"])."\nAlimentazione:  ".ucfirst($_SESSION["alimentazione"])."\nPrezzo:  ".$_SESSION["prezzo"]."\n"."_._._._._._._._._._._"."\nLa tua ricerca ha prodotto ".$_SESSION['count']."  risultati"."\n"."_._._._._._._._._._._\n".$_SESSION['total_elements'][$_SESSION["$i"]]);
 }
