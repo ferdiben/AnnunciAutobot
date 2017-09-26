d1565b7b2640759dcb84d9e99fe257b2a418be0d
federicosozio <sozio.federico@gmail.com>
2017-09-26 20:05:43 +0200

    Update update.php

--- update.php
+++ update.php
@@ -25,10 +25,10 @@ $text = $result["message"]["text"];
 $text = trim($text);
 $text = strtolower($text);
 
-//if($_SESSION["$h"] == 0){
-  //  $_SESSION["$h"]++;
+if($_SESSION["$h"] == 0){
+    $_SESSION["$h"]++;
     Parametri($text, $chat_id);
-//}
+}
 
 $questions = setParametri();
 
@@ -45,7 +45,7 @@ $keyb = $telegram->buildInlineKeyBoard($option);
 
 if ($text === "/start" || $text === "/nuova_ricerca" || $callback_query["data"] === "new" || (!isset($_SESSION["marca"]) && !isset($_SESSION["modello"]) && !isset($_SESSION["regione"]) && !isset($_SESSION["provincia"]) && !isset($_SESSION["alimentazione"]) && !isset($_SESSION["prezzo"]))){
         $_SESSION["$i"]=0;
-   // $_SESSION["$h"]=0;
+    $_SESSION["$h"]=0;
     $content = array('chat_id' => $chat_id, 'text' => "Ciao ".$username."! Inserisci l'auto da cercare");
 } else {
 $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Parametri Ricerca:"."\nMarca:  ".ucfirst($_SESSION["marca"])."\nModello:  ".ucfirst($_SESSION["modello"])."\nRegione:  ".ucfirst($_SESSION["regione"])."\nProvincia:  ".ucfirst($_SESSION["provincia"])."\nAlimentazione:  ".ucfirst($_SESSION["alimentazione"])."\nPrezzo:  ".$_SESSION["prezzo"]."\n"."_._._._._._._._._._._"."\nLa tua ricerca ha prodotto ".$_SESSION['count']."  risultati"."\n"."_._._._._._._._._._._\n".$_SESSION['total_elements'][$_SESSION["$i"]]);
