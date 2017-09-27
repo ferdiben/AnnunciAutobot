fed145c476cfd286eec80381aad2ec67547aa3c6
federicosozio <sozio.federico@gmail.com>
2017-09-27 11:02:56 +0200

    Update update.php

--- update.php
+++ update.php
@@ -25,10 +25,8 @@ $text = $result["message"]["text"];
 $text = trim($text);
 $text = strtolower($text);
 
-if($_SESSION["$h"] == 0){
-    $_SESSION["$h"]++;
-    Parametri($text, $chat_id); 
-}
+Parametri($text, $chat_id); 
+
 
 $questions = setParametri();
 
@@ -83,7 +81,6 @@ $keyb = $telegram->buildInlineKeyBoard($option);
 }
 if ($text === "/start" || $text === "/nuova_ricerca" || $callback_query["data"] === "new" || (!isset($_SESSION["marca"]) && !isset($_SESSION["modello"]) && !isset($_SESSION["regione"]) && !isset($_SESSION["provincia"]) && !isset($_SESSION["alimentazione"]) && !isset($_SESSION["prezzo"]))){
         $_SESSION["$i"]=0;
-    $_SESSION["$h"]=0;
     $content = array('chat_id' => $chat_id, 'text' => $saluto.$username."! Inserisci l'auto da cercare");
 } else {
 $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Stai Cercando:"."\n".ucfirst($_SESSION["marca"])." ".ucfirst($_SESSION["modello"])." ".$regione." ".$provincia." ".$alimentazione." ".$prezzo."\n"."_._._._._._._._._._._"."\nIn base alle tue richieste ho trovato ".$_SESSION['count']."  annunci"."\n"."_._._._._._._._._._._\n".$_SESSION['total_elements'][$_SESSION["$i"]]);
