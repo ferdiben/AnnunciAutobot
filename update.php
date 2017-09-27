aad6ab56476af6101d3750c99a5377335f34173e
federicosozio <sozio.federico@gmail.com>
2017-09-27 11:23:32 +0200

    Update update.php

--- update.php
+++ update.php
@@ -30,6 +30,7 @@ Parametri($text, $chat_id);
 
 $questions = setParametri();
 
+
 if($data >= 4 && $data <= 13){
 $saluto = "Buongiorno ";
 }else{
@@ -41,17 +42,16 @@ if($callback_query["data"] === "new" || $text === "/nuova_ricerca"){
     $_SESSION["$i"] = $_SESSION["$i"] + 2;
 } elseif($text === "si"){
     $_SESSION["$i"]++;
-    $_SESSION["$h"]++;
 } elseif($callback_query["data"] === "skip" && (intval($_SESSION["$i"])%2 != 0)){
      $_SESSION["$i"]++;
 } elseif($text === "no" && (intval($_SESSION["$i"])%2 != 0)){
      $_SESSION["$i"]++;
 } elseif($text === "no" && (intval($_SESSION["$i"])%2 == 0)){
     $_SESSION["$i"] = $_SESSION["$i"] + 2;
-} elseif($_SESSION["$h"] != 0){
+} elseif($var == true){
    $_SESSION["$i"]--;
 }
-
+$var=true;
 if(isset($_SESSION["regione"])){
 $regione = "in ".ucfirst($_SESSION["regione"]);
 } else{
@@ -84,8 +84,7 @@ $keyb = $telegram->buildInlineKeyBoard($option);
 }
 if ($text === "/start" || $text === "/nuova_ricerca" || $callback_query["data"] === "new" || (!isset($_SESSION["marca"]) && !isset($_SESSION["modello"]) && !isset($_SESSION["regione"]) && !isset($_SESSION["provincia"]) && !isset($_SESSION["alimentazione"]) && !isset($_SESSION["prezzo"]))){
         $_SESSION["$i"]=0;
-        $_SESSION["$h"]=0;
-
+$var=false;
     $content = array('chat_id' => $chat_id, 'text' => $saluto.$username."! Inserisci l'auto da cercare");
 } else {
 $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Stai Cercando:"."\n".ucfirst($_SESSION["marca"])." ".ucfirst($_SESSION["modello"])." ".$regione." ".$provincia." ".$alimentazione." ".$prezzo."\n"."_._._._._._._._._._._"."\nIn base alle tue richieste ho trovato ".$_SESSION['count']."  annunci"."\n"."_._._._._._._._._._._\n".$_SESSION['total_elements'][$_SESSION["$i"]]);
